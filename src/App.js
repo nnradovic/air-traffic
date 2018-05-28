3b416fc60dd742188b5f009d1a2f5d1ecc85524c
nnradovic <nnradovic@gmail.com>
2018-05-28 12:57:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div class="btn btn-primary">Dugme</div>
+      </div>
+    );
+  }
+}
+
+export default App;
